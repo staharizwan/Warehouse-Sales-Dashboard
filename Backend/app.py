27641e7725c8606f32f34db5259a4e1375201d22
Syed Taha Rizwan <s.taha.rizwan@gmail.com>
2026-09-12 11:44:27 +0200

    CORS, vercel allowed!

--- Backend/app.py
+++ Backend/app.py
@@ -19,7 +19,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173",
+                   "http://https://warehouse-sales-dashboard-blond.vercel.app/"
+                ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
